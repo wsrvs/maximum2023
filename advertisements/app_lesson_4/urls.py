f7b286d32fdd0cb11f32f20853f28730a6cbe4bc
wsrvs <122815322+wsrvs@users.noreply.github.com>
2023-10-14 22:23:54 +0400

    Add files via upload

---
+++ advertisements/app_lesson_4/urls.py
@@ -0,0 +1,6 @@
+﻿from django.urls import path
+from .views import index
+
+urlpatterns = [
+    path('lesson_4', index),
+]
---
+++ advertisements/app_lesson_4/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(reqvest):
+    return HttpResponse('«Домашка по 4 занятию»!')
+
+
+
+
+# Create your views here.
